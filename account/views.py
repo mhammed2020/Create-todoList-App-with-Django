70634c22bbdd6f3ba80dc924ffd98d92b179f76c
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-26 17:45:17 +0100

    Render data  in template from context view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def index(request):
-    
+    tasks = Task.objects.all()
+    context = {'tasks':tasks}
 
-    return render(request,'account/list.html',{})
+    return render(request,'account/list.html',context)
